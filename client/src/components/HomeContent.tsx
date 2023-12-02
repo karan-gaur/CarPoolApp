f7a40ba4acd487a729551d6456d0691a925d4009
phet2309 <phet2309@gmail.com>
2023-12-02 16:37:30 -0500

    Updated animation

--- client/src/components/HomeContent.tsx
+++ client/src/components/HomeContent.tsx
@@ -7,8 +7,8 @@ const HomeContent = () => {
             <div className='w-full flex items-center justify-center overflow-hidden max-sm:flex-col'>
                 <CarpoolAnimation />
                 <div className='w-[30%] ml-10 max-sm:w-[80%] max-sm:ml-0'>
-                    <Button width='w-full' text='Search Ride' onClick={() => console.log('Button CLicked') } />
-                    <Button width='w-full' text='Publish Ride' onClick={() => console.log('Button CLicked') } />
+                    <Button width='w-full' text='Search Ride' onClick={() => console.log('Button CLicked')} />
+                    <Button width='w-full' text='Publish Ride' onClick={() => console.log('Button CLicked')} />
                 </div>
             </div>
             <div className="flex justify-between flex-wrap max-w-[80%] mx-auto pb-10 space-x-5 max-sm:flex-col max-sm:items-center">
@@ -21,7 +21,13 @@ const HomeContent = () => {
                 <div className="flex-1 max-w-[30%] text-left max-sm:text-center max-sm:max-w-[90%] max-sm:my-10">
                     <div className="text-3xl">Values That Drive Us Together</div>
                     <div className='mt-5'>
-                        Carpool Connect is driven by a commitment to community collaboration, aiming to unite individuals for shared journeys that reduce traffic congestion and environmental impact. Our core values prioritize efficiency through optimal route planning and timely pickups, ensuring convenience for users while minimizing travel time. Embracing sustainability, we actively promote eco-friendly practices, contributing to a greener environment and a collective effort to reduce carbon footprints.
+                        Carpool Connect unites individuals for shared journeys, reducing traffic congestion and environmental impact. Our values prioritize efficient route planning and timely pickups for user convenience. Join us in fostering community collaboration and promoting eco-friendly commuting.
+
+
+
+
+
+
                     </div>
                 </div>
                 <div className="flex-1 max-w-[30%] text-left max-sm:text-center max-sm:max-w-[90%] max-sm:my-10">
