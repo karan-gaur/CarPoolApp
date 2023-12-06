7f06286c4d8f0330edbfeb0d52dce14f51ac9d9c
phet2309 <phet2309@gmail.com>
2023-12-06 14:02:53 -0500

    Added cors middleware

--- backend/project/app.js
+++ backend/project/app.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var express = require("express");
+const cors = require('cors');
 var cookieParser = require("cookie-parser");
 var { transports } = require("winston");
 var expressWinston = require("express-winston");
@@ -9,6 +10,7 @@ var ridesRouter = require("./routes/ride");
 var indexRouter = require("./routes/index");
 
 var app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
