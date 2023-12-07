be15b7f4760c4dfd8c42c6cdd27925e15e15ac4a
kalli-bhaskar <99026519+kalli-bhaskar@users.noreply.github.com>
2023-12-07 12:12:33 -0500

    Update RideStatus.tsx

--- client/src/pages/RideStatus.tsx
+++ client/src/pages/RideStatus.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Transition from '../components/Transition';
+import { Link } from 'react-router-dom';
+import './RideStatus.css';
 
 const RideStatus = () => {
   return (
     <>
       <Transition />
-      <div>RideStatus</div>
+      <div className='ongoing-container'>
+        <h1>No ongoing rides.</h1>
+        <h3><Link to={"/ride-search"}>Click here</Link> to search for a new ride.</h3>
+      </div>
     </>
   )
 }
